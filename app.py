032943163b7d26f00fa1c6a7bbeaf2ae67124a05
TheNobody-12 <sarthak.kce20@sot.pdpu.ac.in>
2023-09-05 21:48:57 +0530

    updated

--- app.py
+++ app.py
@@ -709,7 +709,7 @@ def log_Testimonials():
     return redirect(url_for('dashboard', _anchor='Testimonials'))
 
 def main():
-    app.run(debug=True,port = 8080)
+    app.run(debug=True,port = 80)
 
 if __name__ == "__main__":
     main()
